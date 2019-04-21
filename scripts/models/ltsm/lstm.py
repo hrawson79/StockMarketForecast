a5d553a61775d2e83bb6891ee24c83581bf82b15
Hunter Rawson <rawsonhunter79@gmail.com>
2019-04-21 00:14:06 -0400

    updates to the model

--- scripts/models/ltsm/lstm.py
+++ scripts/models/ltsm/lstm.py
@@ -14,7 +14,7 @@ KEEP_PROB = 0.8
 LSTM_SIZE = 128
 NUM_LAYERS = 1
 INIT_EPOCH = 5
-MAX_EPOCH = 10
+MAX_EPOCH = 100
 VECTOR_SIZE = 6
 
 def chunks(l, n):
@@ -32,44 +32,42 @@ class LSTM:
             self.inputs = tf.placeholder(tf.float32, [None, NUM_STEPS, VECTOR_SIZE])
             self.targets = tf.placeholder(tf.float32, [None, TARGET_SIZE])
             self.learning_rate = tf.placeholder(tf.float32, None)
-            
+
             def _create_one_cell():
-                lstm_cell = tf.contrib.rnn.LSTMCell(LSTM_SIZE)
+                return tf.contrib.rnn.LSTMCell(LSTM_SIZE)
                 if KEEP_PROB < 1.0:
                     return tf.contrib.rnn.DropoutWrapper(lstm_cell, output_keep_prob=KEEP_PROB)
-                else:
-                    return lstm_cell
-            
-            cell = tf.contrib.rnn.MultiRNNCell([_create_one_cell() for _ in range(NUM_LAYERS)], state_is_tuple=True) if NUM_LAYERS > 1 else _create_one_cell()
-            
+
+            self.cell = tf.contrib.rnn.MultiRNNCell([_create_one_cell() for _ in range(NUM_LAYERS)], state_is_tuple=True) if NUM_LAYERS > 1 else _create_one_cell()
+
             #input_weights = tf.Variable(tf.truncated_normal([VECTOR_SIZE]))
             #input_bias = tf.Variable(tf.constant(0.01, shape=[VECTOR_SIZE]))
-            
+
             #input_layer = tf.matmul(inputs, input_weights) + input_bias
-            
-            val, _ = tf.nn.dynamic_rnn(cell, self.inputs, dtype=tf.float32)
-            
-            val = tf.transpose(val, [1,0,2])
-            
-            last = tf.gather(val, int(val.get_shape()[0]) - 1, name="last_lstm_output")
-            
-            weight = tf.Variable(tf.truncated_normal([LSTM_SIZE, VECTOR_SIZE]))
-            bias = tf.Variable(tf.constant(0.01, shape=[VECTOR_SIZE]))
-            self.prediction = tf.matmul(last, weight) + bias
-            self.pred = self.prediction
-            
+
+            self.val, _ = tf.nn.dynamic_rnn(self.cell, self.inputs, dtype=tf.float32)
+
+            self.val = tf.transpose(self.val, [1,0,2])
+
+            self.last = tf.gather(self.val, int(self.val.get_shape()[0]) - 1, name="last_lstm_output")
+
+            self.weight = tf.Variable(tf.truncated_normal([LSTM_SIZE, VECTOR_SIZE]))
+            self.bias = tf.Variable(tf.constant(0.01, shape=[VECTOR_SIZE]))
+            self.prediction = tf.matmul(self.last, self.weight) + self.bias
+            self.pred = tf.reduce_mean(self.prediction)
+
             self.loss = tf.reduce_mean(tf.square(self.prediction - self.targets))
-            optimizer = tf.train.RMSPropOptimizer(self.learning_rate)
-            self.minimize = optimizer.minimize(self.loss)
-    
+            self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate)
+            self.minimize = self.optimizer.minimize(self.loss)
+
     def train(self, train_inputs, train_targets):
-            
+
         learning_rates_to_use = [INIT_LEARNING_RATE * (LEARNING_RATE_DECAY ** max(float(i + 1 - INIT_EPOCH), 0.0)) for i in range(MAX_EPOCH)]
 
         # Train graph
         with tf.Session(graph=self.graph) as sess:
             tf.global_variables_initializer().run()
-        
+
             for epoch_step in range(MAX_EPOCH):
                 self.current_lr = learning_rates_to_use[epoch_step]
                 for batch_X, batch_Y in zip(list(chunks(train_inputs, BATCH_SIZE)), list(chunks(train_targets, BATCH_SIZE))):
@@ -99,7 +97,7 @@ class LSTM:
                 data_frame[0].append(i)
                 data_frame[1].append(np.ravel(summary3))
                 data_frame[2].append(np.ravel(summary2))
-            
+
         fig, ax = plt.subplots(figsize=(16,9))
 
         data_frame[2] = np.multiply(data_frame[2],1)
--- test.py
+++ test.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     tickers = ['^GSPC']
 
     # Date range of data to be downloaded
-    start_date = '2018-06-02'
+    start_date = '2016-02-02'
     end_date = '2019-03-15'
 
     INPUT_SIZE = 1
@@ -27,21 +27,21 @@ if __name__ == "__main__":
     LSTM_SIZE = 128
     NUM_LAYERS = 1
     INIT_EPOCH = 5
-    MAX_EPOCH = 10
+    MAX_EPOCH = 100
     VECTOR_SIZE = 6
 
     stock_1 = stock_data.stock(tickers, start_date, end_date)
     data_1 = model_data.model_data(stock_1, INPUT_SIZE, NUM_STEPS)
-
+    #print(stock_1.shape)
 
     # Initialize graph
     lstm_1 = lstm.LSTM()
-    
+
 
     # Define the graph
     lstm_1.define()
 
-    
+    #print(data_1.test_targets_)
     # Train graph
     lstm_1.train(data_1.train_inputs_, data_1.train_targets_)
 
